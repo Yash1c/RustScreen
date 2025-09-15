fe2f3b01f3bb1c0aec7539fe1856c9c8cf500a3f
Yash1c <guitrevisan2006@gmail.com>
2025-09-15 00:02:57 -0300

    feat: first ISO build – system now bootable

---
+++ rustscreen-app/target/x86_64-unknown-linux-musl/release/deps/rustscreen_app-449260ac9a2302a0.d
@@ -0,0 +1,5 @@
+/media/yashic/HD/RustScreen/rustscreen-app/target/x86_64-unknown-linux-musl/release/deps/rustscreen_app-449260ac9a2302a0.d: src/main.rs
+
+/media/yashic/HD/RustScreen/rustscreen-app/target/x86_64-unknown-linux-musl/release/deps/rustscreen_app-449260ac9a2302a0: src/main.rs
+
+src/main.rs:
---
+++ rustscreen-app/target/x86_64-unknown-linux-musl/release/rustscreen-app.d
@@ -0,0 +1,1 @@
+/media/yashic/HD/RustScreen/rustscreen-app/target/x86_64-unknown-linux-musl/release/rustscreen-app: /media/yashic/HD/RustScreen/rustscreen-app/src/main.rs
